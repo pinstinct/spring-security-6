3ce0b8c16959a19e853b617f812ae0223ff7b082
pinstinct <contact@limhm.dev>
2024-08-02 11:19:58 +0900

    세션 생성 정책 - sessionManagement().sessionCreationPolicy()

--- src/main/java/dev/limhm/security6/SecurityConfig.java
+++ src/main/java/dev/limhm/security6/SecurityConfig.java
@@ -2,10 +2,9 @@ package dev.limhm.security6;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,18 +16,13 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/login").permitAll()
             .anyRequest().authenticated())
-        // spring security 는 post, put, delete 등 조회가 아닌 요청에서는 csrf 토큰 값을 요구
-        // 테스트를 위해 해당 기능을 비활성화
-        .csrf(AbstractHttpConfigurer::disable);
+        .formLogin(Customizer.withDefaults())
+        .sessionManagement(
+            httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS))
+    ;
     return http.build();
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-      throws Exception {
-    return configuration.getAuthenticationManager();
   }
 
   @Bean
